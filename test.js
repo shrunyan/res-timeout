25e6cae6ef660af207b72195958c396dd48cad1f
stuart runyan <shrunyan@gmail.com>
2017-02-08 18:19:53 -0800

    added: test for timedout property

--- test.js
+++ test.js
@@ -3,6 +3,9 @@
 var test = require('ava')
 var resTimeout = require('./index')
 
+const req = {}
+const next = () => {}
+
 test('throws a TypeError if timeout length not defined', (t) => {
   try {
     resTimeout()
@@ -25,4 +28,20 @@ test('Returns a middleware function', (t) => {
   const middleware = resTimeout(5000)
   const isFunc = {}.toString.call(middleware)
   t.is(isFunc, '[object Function]')
+})
+
+test.cb('res.timedout gets set to true', (t) => {
+  var resMock = {
+    setTimeout: (time, cb) => {
+      setTimeout(cb, time)
+    },
+    sendStatus: () => {},
+    timedout: null
+  }
+  const middleware = resTimeout(10, () => {
+    t.truthy(resMock.timedout)
+    t.end()
+  })
+
+  middleware(req, resMock, next)
 })
